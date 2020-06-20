f292982c7f4452923750a168844d3626478c68ca
Mbadak <m.badak99@gmail.com>
2020-06-20 21:50:52 +0200

    son güncelleme yapildi

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,14 +4,13 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
+		
 		System.out.println("second commit from local repository");
 	
 		System.out.println("ikin ci kez cakisma yapmak istiyoruz");
 		
-
-		
-
 		System.out.println("islem tamam");
+		
 		System.out.println("first commit from remote repository");
 		
  
